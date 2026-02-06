a62daefafec3b08317fb84b7f7565d537db3a195
yuferev <yuferev@tochka.com>
2026-02-06 19:31:29 +0500

    исправлены ошибки

--- web/server.js
+++ web/server.js
@@ -1,3 +1,6 @@
+// Загрузка переменных окружения из .env
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
